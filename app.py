49169a6282cded992807587033ad05934c1dae47
nikolmichaelidou <nikolmichaelidou.nm@gmail.com>
2022-11-09 01:25:31 +0000

    made app.run, added a page, fixed typo in notebooks

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
+
+app = MultiPage(app_name="Cherry Leaves Mildew Detector")
+
+#page scripts
+from app_pages.summary_page import summary_page_body
+
+
+app.run()
---
+++ app_pages/summary_page.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+
+
+def summary_page_body():
+    st.title('Dataset summary')
+    st.text(
+        'This dataset contains 2104 images'
+    )
